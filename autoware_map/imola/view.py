e02762563b5641c4bab83ca9e459b11c8d2e7748
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2023-05-04 22:06:13 +0200

    Exported map data in json format

--- autoware_map/imola/view.py
+++ autoware_map/imola/view.py
@@ -1,19 +1,40 @@
 import cv2
 import numpy as np
+import json
 
-def export2d(map_image):
-    map_to_export = np.zeros(map_image.shape, dtype=int)
+def export2d(map_image, replace_val: np.uint8, max_val: np.uint8):
+    map_to_export = map_image
     for i in range(map_image.shape[0]):
         for j in range(map_image.shape[1]):
-            if (map_image[i, j] == 0):
-                map_to_export[i, j] = 100
+            if (map_image[i, j] == replace_val):
+                map_to_export[i, j] = max_val
     return map_to_export
     
 
 im = cv2.imread('imola.pgm',-1) 
 ret,thresh2 = cv2.threshold(im,240,255,cv2.THRESH_BINARY_INV)
-map_export = export2d(thresh2)
-print(map_export)
 
-cv2.imshow('track', thresh2)
+# erode
+kernel1 = np.ones((25, 25), np.uint8)
+image = cv2.dilate(thresh2, kernel1)
+
+ret, thresh3 = cv2.threshold(image,240,255,cv2.THRESH_BINARY_INV)
+
+map_known = export2d(thresh2, 255, 50)
+
+image_replaced = thresh3 + map_known
+
+map_export = export2d(image_replaced, 0, 100)
+
+data = {
+    'heigth': map_export.shape[0],
+    'width': map_export.shape[1],
+    'map': map_export.tolist()
+}
+
+json_string = json.dumps(data)
+with open('json_data.json', 'w') as outfile:
+    outfile.write(json_string)
+
+cv2.imshow('track', image_replaced)
 cv2.waitKey(0)
